105dbc82e223a30a9b5563b8fa956373e0084af9
francamaradev <francamaradev@gmail.com>
2025-04-10 21:27:18 -0300

    fix: coso

--- src/app/api/products/[productId]/route.ts
+++ src/app/api/products/[productId]/route.ts
@@ -1,25 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  context: { params: { productId: string } } // Fix the params type
-) {
+type Params = {
+  params: {
+    productId: string;
+  };
+};
+
+export async function GET(req: NextRequest, { params }: Params) {
   try {
-    const productId = parseInt(context.params.productId, 10);
+    const productId = parseInt(params.productId, 10);
     console.log("Product ID:", productId);
 
     if (!productId) {
       return NextResponse.json(
-        { message: "Missing productId in URL" },
+        { message: "Missing or invalid productId in URL" },
         { status: 400 }
       );
     }
 
     const product = await prisma.product.findFirst({
-      where: { uniqueCode: { equals: productId } },
+      where: { sku: productId.toString() },
     });
-    
 
     if (!product) {
       return NextResponse.json(
@@ -32,7 +34,7 @@ export async function GET(
   } catch (error) {
     console.error("Error en la API de productos:", error);
     return NextResponse.json(
-      { message: "Error al obtener el producto", error },
+      { message: "Error al obtener el producto" },
       { status: 500 }
     );
   }
